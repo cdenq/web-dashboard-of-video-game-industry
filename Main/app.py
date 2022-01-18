e3c7457240612412e378bc2c6e886b505b6e29be
Christopher K Denq <74934154+cdenq@users.noreply.github.com>
2022-01-17 20:23:30 -0500

    Chris update main.js, google.js, app.py

--- Main/app.py
+++ Main/app.py
@@ -1,3 +1,7 @@
+#------------------------
+# DEPENDENCIES + SETUP
+#------------------------
+
 # dependencies
 from flask import Flask, render_template
 import pymongo as pym
@@ -12,15 +16,44 @@ db = client.trending_games_db
 # setup Flask app
 app = Flask(__name__)
 
+#------------------------
+# DATA IMPORT
+#------------------------
+# create list of all the collection names
+collection_names = db.list_collection_names()
+
+# dump all collections' data into one mega list
+# big_data = [list(db[i].find()) for i in collection_names]
+
+# create individual variables for each collection
+steam_data = list(db['steam_charts'].find())
+twitch_data = list(db['twitch'].find())
+gline_data = list(db['gtrends_multiline'].find())
+ggeo_data =  list(db['gtrends_geo'].find())
+yahoo_data = list(db['y_finance'].find())
+
+#------------------------
+# ROUTE CODE
+#------------------------
 # home route
 @app.route('/')
 def home():
-    return render_template('static/html/index.html', var=var)
+    return render_template('static/html/index.html', \
+        steamData = steam_data, \
+        twitchData = twitch_data, \
+        googleLineData = gline_data, \
+        googleGeoData = ggeo_data, \
+        yahooData = yahoo_data)
 
 # dashboard route
 @app.route('/dashboard')
 def dashboard():
-    return render_template('static/html/dashboard.html', var=var)
+    return render_template('static/html/dashboard.html', \
+        steamData = steam_data, \
+        twitchData = twitch_data, \
+        googleLineData = gline_data, \
+        googleGeoData = ggeo_data, \
+        yahooData = yahoo_data)
 
 if __name__ == "__main__":
     app.run(debug=True)
