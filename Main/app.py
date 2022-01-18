a5ce796238c5b0250933d445970debdc5f676bd5
Christopher K Denq <74934154+cdenq@users.noreply.github.com>
2022-01-17 19:03:26 -0500

    Chris update js

--- Main/app.py
+++ Main/app.py
@@ -7,7 +7,7 @@ conn = 'mongodb://localhost:27017'
 client = pym.MongoClient(conn)
 
 # setup mongo variables
-db = client.___
+db = client.trending_games_db
 
 # setup Flask app
 app = Flask(__name__)
@@ -15,12 +15,12 @@ app = Flask(__name__)
 # home route
 @app.route('/')
 def home():
-    return render_template('index.html', var=var)
+    return render_template('static/html/index.html', var=var)
 
 # dashboard route
 @app.route('/dashboard')
 def dashboard():
-    return render_template('dashboard.html', var=var)
+    return render_template('static/html/dashboard.html', var=var)
 
 if __name__ == "__main__":
     app.run(debug=True)
---
+++ Main/static/js/googletrends.js
@@ -0,0 +1,47 @@
+//---------------------------------------------------------
+//GOOGLE MULTILINE INIT
+//---------------------------------------------------------
+function firstGoogleLine(data) {
+    let googleLineTitle = `${Game} Search Relevancy Over Time`
+
+    let trace1 = {
+        x: data.months,
+        y: data.,
+        text: data.months,
+        type:'line'
+    };
+    let googleLineTraceData = [trace1]
+
+    let googleLineLayout = {
+        title: googleLineTitle
+    };
+
+    Plotly.restyle('googleDiv', 'value')
+};
+
+//---------------------------------------------------------
+//GOOGLE MULTILINE RESTYLE
+//---------------------------------------------------------
+function googleLine(data) {
+    let googleLineTitle = `${Game} Search Relevancy Over Time`
+
+    data.
+
+    let trace1 = {
+
+    };
+    let googleLineTraceData = [trace1]
+
+    let googleLineLayout = {
+        title: googleLineTitle
+    };
+
+    Plotly.restyle('googleDiv', 'value', [data])
+};
+
+
+//---------------------------------------------------------
+//GOOGLE GEOMAP
+//---------------------------------------------------------
+function ggeo(data) {
+};
