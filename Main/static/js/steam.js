e148ba205fd91e58ee3a16f9024a8c1746f45822
Nicole Van Driss <nvandriss@gmail.com>
2022-01-19 11:54:36 -0500

    steam.js update

--- Main/static/js/steam.js
+++ Main/static/js/steam.js
@@ -3,6 +3,47 @@
 //---------------------------------------------------------
 function graphSteam() {
 };
+var HPdata = [{
+    values: [406030282,354059895,141938423,98405225,73619062],
+    labels: ['Counter Strike: Global Offensive', 'Dota 2', 'PUBG: Battlegrounds', 'Apex Legends', 'Grand Theft Auto V'],
+    type: 'pie',
+    title: 'Hours Played'
+  }];
+  
+  var layout = {
+    height: 400,
+    width: 500
+  };
+  
+  Plotly.newPlot('myDiv', HPdata, layout);
+
+  var PPdata = [{
+    values: [906996,760295,696704,251595,163339],
+    labels: ['Counter Strike: Global Offensive', 'Dota 2', 'PUBG: Battlegrounds', 'Apex Legends', 'Grand Theft Auto V'],
+    type: 'pie',
+    title: 'Peak Players'
+  }];
+  
+  var layout = {
+    height: 400,
+    width: 500
+  };
+  
+  Plotly.newPlot('myDiv', PPdata, layout);
+
+  var CPdata = [{
+    values: [809631,661120,565534,240196,123172],
+    labels: ['Counter Strike: Global Offensive', 'Dota 2', 'PUBG: Battlegrounds', 'Apex Legends', 'Grand Theft Auto V'],
+    type: 'pie',
+    title: 'Current Players'
+  }];
+  
+  var layout = {
+    height: 400,
+    width: 500
+  };
+  
+  Plotly.newPlot('myDiv', CPdata, layout);
 
 //---------------------------------------------------------
 //STEAM MULTILINE RESTYLE
