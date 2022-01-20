d536df90179dc3ea67134d5e2001b21704182843
Christopher K Denq <74934154+cdenq@users.noreply.github.com>
2022-01-19 21:28:39 -0500

    WED FINAL PUSH

--- Main/app.py
+++ Main/app.py
@@ -4,7 +4,9 @@
 # dependencies
 from flask import Flask, render_template
 import pymongo as pym
-from bson.json_util import dumps
+from bson import json_util
+import json
+from flask import jsonify
 
 # setup mongo connection
 conn = 'mongodb://localhost:27017'
@@ -17,23 +19,6 @@ db = client.trending_games_db
 app = Flask(__name__, template_folder='./static/html')
 
 #------------------------
-# DATA IMPORT
-#------------------------
-# create list of all the collection names
-collection_names = db.list_collection_names()
-
-# dump all collections' data into one mega list
-# big_data = [list(db[i].find()) for i in collection_names]
-
-# create individual variables for each collection
-steam_data = list(db['steam_charts'].find())
-twitch_data = list(db['twitch'].find())
-gline_data = list(db['gtrends_multiline'].find())
-ggeo_data =  list(db['gtrends_geo'].find())
-yahoo_data = list(db['y_finance'].find())
-test_var = 5
-
-#------------------------
 # ROUTE CODE
 #------------------------
 # home route
@@ -41,11 +26,30 @@ test_var = 5
 def home():
     return render_template('index.html')
 
+@app.route('/data')
+def data():
+    # create individual variables for each collection
+    # steam_data = [doc for doc in db['steam_charts'].find()]
+    # twitch_data = db['items'].find()
+    # gline_data = db['gtrends_multiline'].find()
+    # ggeo_data =  db['gtrends_geo'].find()
+    # yahoo_data = db['y_finance'].find()
+
+    # steam_data = json.dumps(steam_data, default=json_util.default)
+    steam_data = json.dumps(list(db['steam_charts'].find()), indent = 2)
+    twitch_data = json.dumps(list(db['items'].find()), indent = 2)
+    gline_data = json.dumps(list(db['gtrends_multiline'].find()), indent = 2)
+    ggeo_data = json.dumps(list(db['gtrends_geo'].find()), indent = 2)
+    yahoo_data = json.dumps(list(db['y_finance'].find()), indent = 2)
+
+    # return data
+    return [steam_data, twitch_data, gline_data, ggeo_data, yahoo_data]
+    
+
 # dashboard route
 @app.route('/dashboard')
 def dashboard():
-    # return "hi"
-    return render_template('dashboard.html', steamData = steam_data, twitchData = twitch_data, googleLineData = gline_data, googleGeoData = ggeo_data, yahooData = yahoo_data, testMe = test_var)
+    return render_template('dashboard.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
--- Main/static/js/main.js
+++ Main/static/js/main.js
@@ -1,28 +1,33 @@
 //---------------------------------------------------------
 //INIT / MAIN FUNCTION THAT DOES THE FIRST POPULATE
 //---------------------------------------------------------
-function main() {
-  // Set the default game title in dropdown
-  //APEX
-  document.querySelector("#selDataset").value = 'Apex Legends';
+async function main() {
+  url = '/data';
+  let r = (await fetch(url)).json();
+  console.log(r);
 
-  // Set the default charts
-  testerFunction(testMe);
-  graphGoogleLine(googleLineData);
-  graphGoogleGeo(googleGeoData);
-  graphSteam(steamData);
-  twitchbar();
-  graphYahoo(yahooData);
-  madeIt();
+  // // Set the default game title in dropdown
+  // //APEX
+  // document.querySelector("#selDataset").value = 'Apex Legends';
+
+  // let googleLineData =
+  // // Set the default charts
+  // testerFunction(testMe);
+  // graphGoogleLine(googleLineData);
+  // graphGoogleGeo(googleGeoData);
+  // graphSteam(steamData);
+  // twitchbar();
+  // graphYahoo(yahooData);
+  // madeIt();
 
   // gives dropdown menu interactivity
-  document.querySelector("#selDataset").addEventListener("change", event => {
+  // document.querySelector("#selDataset").addEventListener("change", event => {
     // restyleGoogleLine(event.target.value);
     // restyleGoogleGeo(event.target.value);
     // restyleSteam(event.target.value);
     // restyleTwitch(event.target.value);
     // restyleYahoo(event.target.value);
-  });
+  // });
 };
 
 function madeIt() {
