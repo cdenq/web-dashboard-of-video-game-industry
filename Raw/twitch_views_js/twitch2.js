6a33f6d05494fca4af4c279147ed3fb666e01c1d
Joseph Scuteri <scuterijoseph@gmail.com>
2022-01-19 21:11:14 -0500

    MongoDB to JSON & New Graph
    Converting new MongoDB to JSON. Updating twitch.js graph to make it functional and not static (with hard coded data)

---
+++ Raw/twitch_views_js/twitch2.js
@@ -0,0 +1,33 @@
+//---------------------------------------------------------
+//TWITCH VIEWS
+//---------------------------------------------------------
+function twitchbar() {
+
+var Viewdata = [
+  {
+    x: ['Counter Strike: Global Offensive', 'Dota 2', 'PUBG: Battlegrounds', 'Apex Legends', 'Grand Theft Auto V'],
+    y: [5000,72900,21900,93200,285000],
+    type: 'bar',
+    title: 'Total Views (Rounded to Nearest 1000)'
+  }];
+  
+  var layout = {
+    height: 400,
+    width: 500
+   };      
+      annotation: {
+        annotations: [{
+            type: 'line',
+            mode: 'horizontal',
+            scaleID: 'y-axis-0',
+            value: '95600',
+            borderColor: 'tomato',
+            borderWidth: 1
+        }]
+    }
+  
+  Plotly.newPlot('twitch', Viewdata, layout);
+
+};
+
+twitchbar()
